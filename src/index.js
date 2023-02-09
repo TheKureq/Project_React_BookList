b052313d924f59cf945ea8850175881721c9fcfc
TheKureq <xkurekx@gmail.com>
2023-02-10 00:09:27 +0100

    passing parent objects / methods to child

--- src/index.js
+++ src/index.js
@@ -27,13 +27,32 @@ const books = [
 // COMPONENTS
 // ==========
 const BookList = () => {
+	const getBook = id => {
+		const book = books.find(book => book.id === id)
+		console.log(book)
+	}
 	return (
 		<section className='bookList'>
 			<EventExamples />
 			{books.map(book => {
-				return <Book {...book} key={book.id} />
+				return <Book {...book} key={book.id} getBook={getBook} />
 			})}
 		</section>
+	)
+}
+
+const Book = props => {
+	const { img, title, author, getBook, id } = props
+	// const getSingleBook = () => {
+	// 	getBook(id)
+	// }
+	return (
+		<article className='book'>
+			<img src={img} alt={title} />
+			<h2>{title.toUpperCase()}</h2>
+			<button onClick={() => getBook(id)}>display book id</button>
+			<h4>{author}</h4>
+		</article>
 	)
 }
 
@@ -67,17 +86,6 @@ const EventExamples = () => {
 				</div>
 			</form>
 		</section>
-	)
-}
-
-const Book = props => {
-	const { img, title, author } = props
-	return (
-		<article className='book'>
-			<img src={img} alt={title} />
-			<h2>{title.toUpperCase()}</h2>
-			<h4>{author}</h4>
-		</article>
 	)
 }
 
